e1d051e3af4881fe09a761f5e31b0ce5311cbe7d
MikhailDemin <bahamutffxii@gmail.com>
2021-02-04 00:18:55 +1000

    lesson_5 task solutions restore

--- task_5-5.py
+++ task_5-5.py
@@ -15,3 +15,4 @@ with open('task_5_file.txt',  mode='w+', encoding='utf-8') as the_file:
     the_file.write(" ".join([str(randint(1, 1000)) for _ in range(100000)]))
     the_file.seek(0)
     print(sum(map(int, the_file.readline().split())))
+
